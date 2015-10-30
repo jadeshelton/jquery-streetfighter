73ab83744ebd1b9962ba0e14982cfd3060b3614c
jadeshelton <jnshelton257@yahoo.com>
2015-10-29 22:32:33 -0700

    ryu cool pose

--- js/app.js
+++ js/app.js
@@ -23,10 +23,24 @@ $(document).ready(function() {
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
-  });
+  })
   function playHadouken () {
   	$('#hadouken-sound')[0].volume = 0.5;
   	$('#hadouken-sound')[0].load();
   	$('#hadouken-sound')[0].play();
-  }
+  };
+  $('body').keydown(function() {
+  	var x = event.keyCode;
+  	if (x == 88) {
+  		$('.ryu-still').hide();
+  		$('.ryu-cool').show();
+  	}
+})
+  .keyup(function() {
+  	var x = event.keyCode;
+  	if (x == 88) {
+  		$('.ryu-cool').hide();
+  		$('.ryu-still').show();
+  	}
+  });
 });
